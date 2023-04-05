a051d156c3224a46d08f32efcb6a969ac2b051ed
tomoaki_narukawa <tomo111911111@gmail.com>
2023-04-06 08:31:47 +0900

    cssのscssパターン

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { ColoredMessage } from './components/ColoredMessage';
+import classes from './css/CssModules.module.scss';
 
 export const App = () => {
   console.log('レンダリング');
@@ -16,7 +17,9 @@ export const App = () => {
       <h1 style={{ color: 'red' }}>こんにちは！</h1>
       <ColoredMessage color='blue'>お元気ですか？</ColoredMessage>
       <ColoredMessage color='pink'>元気です!</ColoredMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button className={classes.button} onClick={onClickButton}>
+        ボタン
+      </button>
       <p>{num}</p>
     </>
   );
